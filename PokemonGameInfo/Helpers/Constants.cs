e14bc5f0e5a15b932397c8dc6eb1288187b9192a
PCBarroso <barroso.pc@gmail.com>
2016-12-05 20:50:24 -0200

    .

--- PokemonGameInfo/Helpers/Constants.cs
+++ PokemonGameInfo/Helpers/Constants.cs
@@ -8,6 +8,7 @@ namespace PokemonGameInfo.Helpers
     public class Constants
     {
         public const string LuisSubscriptionKey = "e58700e5492f4d89aa3b99749be3f9df";
+        public const string ExamplePNG = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/131.png";
 
         public const string LuisIdRoot = "70300c18-fe86-490c-9dd5-3f08176042c7";
     }
